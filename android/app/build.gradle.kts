dad2d47397a93087ec95ce0e7f489b6a9df4af28
parkjuho12 <pjhjh2350@gmail.com>
2025-12-09 11:27:50 +0900

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.qrsystem"
+    namespace = "com.pjh.qrsystem"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
     
